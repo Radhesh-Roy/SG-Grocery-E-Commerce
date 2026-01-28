6a107436cfccfc91558653b5efd3406c41531cf8
Radhesh-Roy <radheshroy0011@gmail.com>
2026-01-28 18:41:18 +0600

    login page

--- lib/view/login_page/login_view.dart
+++ lib/view/login_page/login_view.dart
@@ -8,21 +8,39 @@ class LogInViewPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(padding: EdgeInsets.all(10),
-      child: Center(
         child: Column(
-         crossAxisAlignment: CrossAxisAlignment.center,
+         crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset("assets/appbar-image.png", height: 53,),
-                SizedBox(height: 30,),
-                Image.asset("assets/loginimage.png", height: 152,)
-              ],
-            )
+            Center(child: Image.asset("assets/appbar-image.png", height: 53,)),
+            SizedBox(height: 30,),
+            Center(child: Image.asset("assets/loginimage.png", height: 152,)),// image part
+            Text("Login", style: TextStyle(color: Color(0xff55AB60), fontSize: 24, fontWeight: FontWeight.w600),),
+            SizedBox(height: 20,),
+            Text("Email Id", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),),
+            SizedBox(height: 20,),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                hintText: "Enter Your Email ID", hintStyle: TextStyle(color: Color(0xff858FAD))
+              ),
+            ),
+            SizedBox(height: 20,),
+            Text("Password", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),),
+            SizedBox(height: 20,),
+            TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  hintText: "Enter Your Password", hintStyle: TextStyle(color: Color(0xff858FAD))
+              ),
+            ),
+            SizedBox(height: 20,),
+
           ],
         ),
-      ),
 
       )
     );
