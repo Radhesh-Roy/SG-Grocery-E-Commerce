4f085a73b020af17b310a469199c3f0060178688
Radhesh-Roy <radheshroy0011@gmail.com>
2026-03-10 23:38:53 +0600

    add

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -245,6 +245,7 @@ class HomeViewPage extends StatelessWidget {
                     child: Text("Explore All", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Color(0xff55AB60)),))
               ],
             ),// Featured Item
+
             SizedBox(height: 10,),
             SizedBox(
                 height: 200,
