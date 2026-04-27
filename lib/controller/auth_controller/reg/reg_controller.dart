e9ca01563bbbdd504e4ff16d068a9844817ed7d8
Radhesh-Roy <radheshroy0011@gmail.com>
2026-04-27 16:32:09 +0600

    password visibility

--- lib/controller/auth_controller/reg/reg_controller.dart
+++ lib/controller/auth_controller/reg/reg_controller.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
+import '../../../firebase_service.dart';
+
 class RegController extends GetxController{
+
+  RxBool isVisibile=true.obs;
 
 
   TextEditingController nameC= TextEditingController();
@@ -9,5 +14,31 @@ class RegController extends GetxController{
   TextEditingController passC= TextEditingController();
   TextEditingController confirmPassC= TextEditingController();
   TextEditingController phoneC= TextEditingController();
+
+
+  void setData(){
+    db.ref("registration").child("user").push().set(
+        {
+          "name": nameC.text,
+          "email":emailC.text,
+          "password":passC.text,
+          "confirmPassword":confirmPassC.text,
+          "phone":phoneC.text
+        });
+  }
+
+
+  void checkVisible(){
+    isVisibile.value=!isVisibile.value;
+  }
+  @override
+  void onInit() {
+   setData();
+    super.onInit();
+  }
+
+
+
+
 
 }
