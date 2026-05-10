3de18fd1b97a5dc07780c5563831ad9172b1f103
Radhesh-Roy <radheshroy0011@gmail.com>
2026-05-10 23:42:30 +0600

    add

--- lib/view/registration_page/reg_page.dart
+++ lib/view/registration_page/reg_page.dart
@@ -94,6 +94,7 @@ class RegViewPage extends StatelessWidget {
 
                   Text("Confirm Password", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),),
                   SizedBox(height: 20,),
+
                  Obx(()=> TextFormField(
                    obscureText: controller.isVisibileC.value,
                    validator: (value) {
