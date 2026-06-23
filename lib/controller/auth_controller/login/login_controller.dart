043025306a120cd21ebe008e374c23132ba819fe
Radhesh-Roy <radheshroy0011@gmail.com>
2026-06-23 18:12:12 +0600

    add

--- lib/controller/auth_controller/login/login_controller.dart
+++ lib/controller/auth_controller/login/login_controller.dart
@@ -4,38 +4,29 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:sggrocery/controller/auth_controller/reg/reg_controller.dart';
 import 'package:sggrocery/firebase_service.dart';
+import 'package:sggrocery/view/bottom_bar.dart';
 import 'package:sggrocery/view/home/home_page.dart';
 
 class LoginController extends GetxController{
 
   RxBool isLoading=true.obs;
   RxList userData=[].obs;
-
   TextEditingController logEmail=TextEditingController();
   TextEditingController logPass=TextEditingController();
-
   Future<void> GetData()async{
-
     final snapshot=await db.ref("registration/user").get();
-
     if(snapshot.exists){
-
       Map data= snapshot.value as Map;
       bool isFound=false;
       data.forEach((key, value){
-
         if(logEmail.text== value["email"]&& logPass.text==value["password"]){
-
           isFound=true;
         };
-
       });
-
       if(isFound){
-        Get.snackbar("Login Sucess", "Wellcome");
+        Get.snackbar("Login Success", "Wellcome");
         log("login Success");
-        Get.offAll(HomeViewPage());
-
+        Get.offAll(BottomBarView());
       }
       else{
         Get.snackbar("login Failed", "Enter Correct Email and Password");
@@ -45,7 +36,6 @@ class LoginController extends GetxController{
     else{
       log("No Data");
     }
-
   }
   @override
   void onInit() {
